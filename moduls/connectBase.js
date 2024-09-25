f8d5ed0d4d7f556d5febb960633d6d199bae0e04
dnis0847 <78086750+dnis0847@users.noreply.github.com>
2024-09-25 22:01:41 +0700

    Update connectBase.js

--- moduls/connectBase.js
+++ moduls/connectBase.js
@@ -2,8 +2,8 @@
 
  const connection = mysql.createConnection({
      port: "3306",
-     user: "dnis0847",
-     host: "194.87.84.212",
-     database: "CryptoAPP",
-     password: "5411yaya"
+     user: "",
+     host: "",
+     database: "",
+     password: ""
 });
