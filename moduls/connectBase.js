597ce0d7de05b242868112190bde08f5c9fa6743
dnis0847 <78086750+dnis0847@users.noreply.github.com>
2024-09-25 21:04:47 +0700

    Update connectBase.js

--- moduls/connectBase.js
+++ moduls/connectBase.js
@@ -1,1 +1,9 @@
+ const mysql = require("mysql2");
 
+ const connection = mysql.createConnection({
+     port: "3306",
+     user: "gen_user",
+     host: "null",
+     database: "default_db",
+     password: "5411yaya"
+});
